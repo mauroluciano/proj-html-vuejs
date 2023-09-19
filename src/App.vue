ecc3f537c79c8450b7bcfd05213f63fbbf088280
mauroluciano <mauroluciano911@gmail.com>
2023-09-19 18:12:56 +0200

    add img folder

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,7 @@ import AppHeader from "./components/AppHeader.vue";
 import AppMain from "./components/AppMain.vue";
 import AppFooter from "./components/AppFooter.vue";
 import axios from "axios";
+
 export default {
   components: { AppHeader, AppMain, AppFooter },
 };
