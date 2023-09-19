b66d8a8a9a83740a3083e5fec6beb4d3422c3b9b
mauroluciano <mauroluciano911@gmail.com>
2023-09-19 17:30:09 +0200

    add vuejs

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<script>
+import AppHeader from "./components/AppHeader.vue";
+import AppMain from "./components/AppMain.vue";
+import AppFooter from "./components/AppFooter.vue";
+import axios from "axios";
+export default {
+  components: { AppHeader, AppMain, AppFooter },
+};
+</script>
+
+<template>
+  <AppHeader />
+  <AppMain />
+  <AppFooter />
+</template>
+
+<style lang="scss">
+@use "./assets/styles/general.scss" as *;
+</style>
