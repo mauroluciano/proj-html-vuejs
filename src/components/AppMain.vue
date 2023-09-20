fd1a0e4dd78a27b7b1dab0b95c4a1878ee99877e
mauroluciano <mauroluciano911@gmail.com>
2023-09-20 16:44:19 +0200

    fix

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -68,64 +68,247 @@ import { ref } from "vue";
         </div>
       </div>
     </div>
-    <div class="container-fluid g-0">
-      <div class="ourDoctors py-5">
-        <div class="row text-center p-5">
+
+    <div class="docSection">
+      <div class="container">
+        <div class="ourDoctors text-center py-5">
+          <div class="row p-5">
+            <div class="my-2">
+              <img
+                src="/src/assets/img/images/icon-5.png"
+                class="icon"
+                alt="" />
+            </div>
+
+            <h2>Meet Our Doctors</h2>
+
+            <p>
+              Sed ut perspiciatis unde omnis iste natus error sit voluptatem
+              accusantium doloremque laudantium, totam rem aperiam, eaque ipsa
+              quae ab illo
+            </p>
+
+            <div class="d-flex justify-content-between flex-wrap">
+              <div class="row row-cols-1 col-sm-12 col-md-6 col-lg-3 my-3">
+                <div class="card">
+                  <img
+                    src="/src/assets/img/images/doctor-1-200x206.jpg"
+                    class="card-img-top"
+                    alt="..." />
+                  <div class="card-body text-start">
+                    <h5 class="card-title">Jon Snow</h5>
+                    <sub>Anesthesiologist</sub>
+                    <p class="card-text">
+                      Some quick example text to build on the card title and
+                      make up the bulk of the card's content.
+                    </p>
+                  </div>
+                </div>
+              </div>
+
+              <div class="row row-cols-1 col-sm-12 col-md-6 col-lg-3 my-3">
+                <div class="card">
+                  <img
+                    src="/src/assets/img/images/doctor-2-200x206.jpg"
+                    class="card-img-top"
+                    alt="..." />
+                  <div class="card-body text-start">
+                    <h5 class="card-title">Tony Stark</h5>
+                    <sub>Cardiologist</sub>
+                    <p class="card-text">
+                      Some quick example text to build on the card title and
+                      make up the bulk of the card's content.
+                    </p>
+                  </div>
+                </div>
+              </div>
+              <div class="row row-cols-1 col-sm-12 col-md-6 col-lg-3 my-3">
+                <div class="card">
+                  <img
+                    src="/src/assets/img/images/doctor-3-200x206.jpg"
+                    class="card-img-top"
+                    alt="..." />
+                  <div class="card-body text-start">
+                    <h5 class="card-title">Anna Smith</h5>
+                    <sub>Nurse Practitioner</sub>
+                    <p class="card-text">
+                      Some quick example text to build on the card title and
+                      make up the bulk of the card's content.
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="container">
+      <div class="text-center py-5">
+        <div class="row p-5">
           <div class="my-2">
-            <img src="/src/assets/img/images/icon-5.png" class="icon" alt="" />
+            <img src="/src/assets/img/images/icon-6.png" class="icon" alt="" />
           </div>
 
-          <h2>Meet Our Doctors</h2>
+          <h2>Our Healt Services</h2>
 
           <p>
             Sed ut perspiciatis unde omnis iste natus error sit voluptatem
             accusantium doloremque laudantium, totam rem aperiam, eaque ipsa
             quae ab illo
           </p>
-
-          <div class="">
-            <div class="row justify-content-center">
-              <div class="col-sm-12 col-md-4 col-lg-3">
-                <div class="card" style="width: 18rem">
-                  <img src="..." class="card-img-top" alt="..." />
-                  <div class="card-body">
-                    <h5 class="card-title">Card title</h5>
-                    <p class="card-text">
-                      Some quick example text to build on the card title and
-                      make up the bulk of the card's content.
-                    </p>
-                    <a href="#" class="btn btn-primary">Go somewhere</a>
-                  </div>
+          <div class="d-flex justify-content-between flex-wrap">
+            <div class="col-sm-12 col-md-6 col-lg-4 my-2">
+              <section>
+                <div class="my-2">
+                  <img src="/src/assets/img/images/icon-14.png" alt="" />
                 </div>
-              </div>
-              <div class="col-sm-12 col-md-4 col-lg-3">
-                <div class="card" style="width: 18rem">
-                  <img src="..." class="card-img-top" alt="..." />
-                  <div class="card-body">
-                    <h5 class="card-title">Card title</h5>
-                    <p class="card-text">
-                      Some quick example text to build on the card title and
-                      make up the bulk of the card's content.
-                    </p>
-                    <a href="#" class="btn btn-primary">Go somewhere</a>
-                  </div>
+                <span>Rehabilitation Center</span>
+                <p>
+                  Sed ut perspiciatis unde omnis iste natus error sit voluptatem
+                  accusantium doloremque laudantium, totam rem aperiam, eaque
+                  ipsa quae ab illo
+                </p>
+              </section>
+            </div>
+            <div class="col-sm-12 col-md-6 col-lg-4 my-2">
+              <section>
+                <div class="my-2">
+                  <img src="/src/assets/img/images/icon-15.png" alt="" />
                 </div>
-              </div>
-              <div class="col-sm-12 col-md-4 col-lg-3">
-                <div class="card" style="width: 18rem">
-                  <img src="..." class="card-img-top" alt="..." />
-                  <div class="card-body">
-                    <h5 class="card-title">Card title</h5>
-                    <p class="card-text">
-                      Some quick example text to build on the card title and
-                      make up the bulk of the card's content.
-                    </p>
-                    <a href="#" class="btn btn-primary">Go somewhere</a>
-                  </div>
+                <span>Dental Implants</span>
+                <p>
+                  Sed ut perspiciatis unde omnis iste natus error sit voluptatem
+                  accusantium doloremque laudantium, totam rem aperiam, eaque
+                  ipsa quae ab illo
+                </p>
+              </section>
+            </div>
+            <div class="col-sm-12 col-md-6 col-lg-4 my-2">
+              <section>
+                <div class="my-2">
+                  <img src="/src/assets/img/images/icon-16.png" alt="" />
                 </div>
-              </div>
+                <span>Medicine Research</span>
+                <p>
+                  Sed ut perspiciatis unde omnis iste natus error sit voluptatem
+                  accusantium doloremque laudantium, totam rem aperiam, eaque
+                  ipsa quae ab illo
+                </p>
+              </section>
+            </div>
+            <div class="col-sm-12 col-md-6 col-lg-4 my-2">
+              <section>
+                <div class="my-2">
+                  <img src="/src/assets/img/images/icon-11.png" alt="" />
+                </div>
+                <span>Blood Bank</span>
+                <p>
+                  Sed ut perspiciatis unde omnis iste natus error sit voluptatem
+                  accusantium doloremque laudantium, totam rem aperiam, eaque
+                  ipsa quae ab illo
+                </p>
+              </section>
+            </div>
+            <div class="col-sm-12 col-md-6 col-lg-4 my-2">
+              <section>
+                <div class="my-2">
+                  <img src="/src/assets/img/images/icon-12.png" alt="" />
+                </div>
+                <span>Pharmaceutical Advice</span>
+                <p>
+                  Sed ut perspiciatis unde omnis iste natus error sit voluptatem
+                  accusantium doloremque laudantium, totam rem aperiam, eaque
+                  ipsa quae ab illo
+                </p>
+              </section>
+            </div>
+            <div class="col-sm-12 col-md-6 col-lg-4 my-2">
+              <section>
+                <div class="my-2">
+                  <img src="/src/assets/img/images/icon-13.png" alt="" />
+                </div>
+                <span>Medical Counseling</span>
+                <p>
+                  Sed ut perspiciatis unde omnis iste natus error sit voluptatem
+                  accusantium doloremque laudantium, totam rem aperiam, eaque
+                  ipsa quae ab illo
+                </p>
+              </section>
             </div>
           </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="jumbotronTour container-fluid flex-wrap">
+      <div class="container">
+        <div class="text-center py-5">
+          <div class="row p-5 justify-content-center my-4">
+            <div class="my-2">
+              <img
+                src="/src/assets/img/images/icon-7.png"
+                class="icon"
+                alt="" />
+            </div>
+
+            <h2>Tour Our Facilities</h2>
+
+            <p>
+              Sed ut perspiciatis unde omnis iste natus error sit voluptatem
+              accusantium doloremque laudantium, totam rem aperiam, eaque ipsa
+              quae ab illo
+            </p>
+
+            <div>
+              <img
+                src="/src/assets/img/images/play-icon.png"
+                class="icon"
+                alt="" />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="threeBoxes flex-wrap">
+      <div class="row text-center">
+        <div class="col-sm emergency">
+          <section>
+            <span>Emergency Care</span>
+            <div class="my-2">
+              <img src="/src/assets/img/images/wave-divider.png" alt="" />
+            </div>
+            <p>
+              Sed ut perspiciatis unde omnis iste natus error sit voluptatem
+              accusantium doloremque laudantium, totam rem aperiam, eaque
+            </p>
+          </section>
+        </div>
+        <div class="col-sm expert">
+          <section>
+            <span>Expert Diagnostics</span>
+            <div class="my-2">
+              <img src="/src/assets/img/images/wave-divider.png" alt="" />
+            </div>
+            <p>
+              Sed ut perspiciatis unde omnis iste natus error sit voluptatem
+              accusantium doloremque laudantium, totam rem aperiam, eaque
+            </p>
+          </section>
+        </div>
+        <div class="col-sm superb">
+          <section>
+            <span>Superb Rehabilitation</span>
+            <div class="my-2">
+              <img src="/src/assets/img/images/wave-divider.png" alt="" />
+            </div>
+            <p>
+              Sed ut perspiciatis unde omnis iste natus error sit voluptatem
+              accusantium doloremque laudantium, totam rem aperiam, eaque
+            </p>
+          </section>
         </div>
       </div>
     </div>
