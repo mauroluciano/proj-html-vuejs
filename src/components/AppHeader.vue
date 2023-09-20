c40df0542618417c6da4eccb48c8762608aa0873
mauroluciano <mauroluciano911@gmail.com>
2023-09-20 15:01:30 +0200

    add sections

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -4,43 +4,45 @@ import { ref } from "vue";
 
 <template>
   <header>
-    <div class="navi justify-content-between p-5">
-      <div class="logo">
-        <img src="/src/assets/img/images/medical_logo_1x_light.png" alt="" />
+    <div class="jumbotron container-fluid flex-wrap">
+      <div class="navi justify-content-between p-5 flex-wrap">
+        <div class="logo">
+          <img src="/src/assets/img/images/medical_logo_1x_light.png" alt="" />
+        </div>
+
+        <div class="menu">
+          <ul class="navi align-items-center">
+            <li class="nav-item">
+              <a class="nav-link active" aria-current="page" href="#">Home</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">About</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Departments</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Articles</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link background" href="#">Make Appointment</a>
+            </li>
+          </ul>
+        </div>
       </div>
 
-      <div class="menu">
-        <ul class="navi align-items-center">
-          <li class="nav-item">
-            <a class="nav-link active" aria-current="page" href="#">Home</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">About</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">Departments</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">Articles</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link background" href="#">Make Appointment</a>
-          </li>
-        </ul>
-      </div>
-    </div>
-
-    <div class="container">
-      <div class="message mt-5">
-        <section>
-          <h2>Caring For Life</h2>
-          <p>
-            Kind words can be short and easy to speak, but their echoes are
-            truly endless. Avada Health focuses on you as if it was our own
-            family.
-          </p>
-          <button type="button" class="background">Learn More</button>
-        </section>
+      <div class="container">
+        <div class="message mt-5">
+          <section>
+            <h2>Caring For Life</h2>
+            <p>
+              Kind words can be short and easy to speak, but their echoes are
+              truly endless. Avada Health focuses on you as if it was our own
+              family.
+            </p>
+            <button type="button" class="background">Learn More</button>
+          </section>
+        </div>
       </div>
     </div>
   </header>
--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -2,6 +2,134 @@
 import { ref } from "vue";
 </script>
 
-<template></template>
+<template>
+  <main>
+    <div class="container">
+      <div class="welcome py-5">
+        <div class="row text-center mt-5 mb-2">
+          <h2>Welcome to Avada Health</h2>
+
+          <p>
+            Sed ut perspiciatis unde omnis iste natus error sit voluptatem
+            accusantium doloremque laudantium, totam rem aperiam, eaque ipsa
+            quae ab illo
+          </p>
+        </div>
+
+        <div class="row row-cols-1 text-center boxMessage">
+          <div class="col-sm-12 col-md-6 col-lg-3 my-2">
+            <section>
+              <div class="my-2">
+                <img src="/src/assets/img/images/icon-1.png" alt="" />
+              </div>
+              <span>Same Day Appointments</span>
+              <p>
+                Sed ut perspiciatis unde omnis iste natus error sit voluptatem
+                accusantium
+              </p>
+            </section>
+          </div>
+          <div class="col-sm-12 col-md-6 col-lg-3 my-2">
+            <section>
+              <div class="my-2">
+                <img src="/src/assets/img/images/icon-2.png" alt="" />
+              </div>
+              <span>Same Day Appointments</span>
+              <p>
+                Sed ut perspiciatis unde omnis iste natus error sit voluptatem
+                accusantium
+              </p>
+            </section>
+          </div>
+          <div class="col-sm-12 col-md-6 col-lg-3 my-2">
+            <section>
+              <div class="my-2">
+                <img src="/src/assets/img/images/icon-3.png" alt="" />
+              </div>
+              <span>Same Day Appointments</span>
+              <p>
+                Sed ut perspiciatis unde omnis iste natus error sit voluptatem
+                accusantium
+              </p>
+            </section>
+          </div>
+          <div class="col-sm-12 col-md-6 col-lg-3 my-2">
+            <section>
+              <div class="my-2">
+                <img src="/src/assets/img/images/icon-4.png" alt="" />
+              </div>
+              <span>Same Day Appointments</span>
+              <p>
+                Sed ut perspiciatis unde omnis iste natus error sit voluptatem
+                accusantium
+              </p>
+            </section>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="container-fluid g-0">
+      <div class="ourDoctors py-5">
+        <div class="row text-center p-5">
+          <div class="my-2">
+            <img src="/src/assets/img/images/icon-5.png" class="icon" alt="" />
+          </div>
+
+          <h2>Meet Our Doctors</h2>
+
+          <p>
+            Sed ut perspiciatis unde omnis iste natus error sit voluptatem
+            accusantium doloremque laudantium, totam rem aperiam, eaque ipsa
+            quae ab illo
+          </p>
+
+          <div class="">
+            <div class="row justify-content-center">
+              <div class="col-sm-12 col-md-4 col-lg-3">
+                <div class="card" style="width: 18rem">
+                  <img src="..." class="card-img-top" alt="..." />
+                  <div class="card-body">
+                    <h5 class="card-title">Card title</h5>
+                    <p class="card-text">
+                      Some quick example text to build on the card title and
+                      make up the bulk of the card's content.
+                    </p>
+                    <a href="#" class="btn btn-primary">Go somewhere</a>
+                  </div>
+                </div>
+              </div>
+              <div class="col-sm-12 col-md-4 col-lg-3">
+                <div class="card" style="width: 18rem">
+                  <img src="..." class="card-img-top" alt="..." />
+                  <div class="card-body">
+                    <h5 class="card-title">Card title</h5>
+                    <p class="card-text">
+                      Some quick example text to build on the card title and
+                      make up the bulk of the card's content.
+                    </p>
+                    <a href="#" class="btn btn-primary">Go somewhere</a>
+                  </div>
+                </div>
+              </div>
+              <div class="col-sm-12 col-md-4 col-lg-3">
+                <div class="card" style="width: 18rem">
+                  <img src="..." class="card-img-top" alt="..." />
+                  <div class="card-body">
+                    <h5 class="card-title">Card title</h5>
+                    <p class="card-text">
+                      Some quick example text to build on the card title and
+                      make up the bulk of the card's content.
+                    </p>
+                    <a href="#" class="btn btn-primary">Go somewhere</a>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </main>
+</template>
 
 <style scoped></style>
