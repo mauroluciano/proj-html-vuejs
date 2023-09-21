68b661aa2393fbcc9ebbfdaed96d3dcb7396936f
mauroluciano <mauroluciano911@gmail.com>
2023-09-21 15:27:59 +0200

    add js

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -6,7 +6,7 @@ import { ref } from "vue";
   <footer>
     <div class="container-fluid">
       <div class="row p-5 d-flex flex-wrap">
-        <div class="col-sm my-2">
+        <div class="col-sm my-5">
           <section>
             <img src="/src/assets/img/images/medical_logo_1x_dark.png" alt="" />
             <p class="border-bottom py-4">
@@ -16,29 +16,33 @@ import { ref } from "vue";
               voluptatem
             </p>
             <p>Learn More about Us</p>
+            <div class="d-flex iconsFooter">
+              <img src="/src/assets/img/images/facebook.png" alt="" />
+              <img src="/src/assets/img/images/twitter.png" alt="" />
+              <img src="/src/assets/img/images/youtube.png" alt="" />
+              <img src="/src/assets/img/images/instagram.png" alt="" />
+            </div>
           </section>
         </div>
-        <div class="col-sm my-2">
+        <div class="col-sm my-5">
           <section>
-            <img src="/src/assets/img/images/medical_logo_1x_dark.png" alt="" />
-            <p>
-              Sed ut perspiciatis unde omnis iste natus error sit voluptatem
-              accusantium doloremque laudantium, totam rem aperiam, eaque ipsa
-              quae ab illo ed ut perspiciatis unde omnis iste natus error sit
-              voluptatem
-            </p>
+            <h5>Medical Departments</h5>
+
+            <ul>
+              <li>Cardiology</li>
+              <li>Pediactrics</li>
+              <li>Diabetes Care</li>
+              <li>Pre-natel Care</li>
+              <li>Ultrasound Echocardiogram</li>
+            </ul>
           </section>
         </div>
 
-        <div class="col-sm my-2">
+        <div class="col-sm my-5">
           <section>
-            <img src="/src/assets/img/images/medical_logo_1x_dark.png" alt="" />
-            <p>
-              Sed ut perspiciatis unde omnis iste natus error sit voluptatem
-              accusantium doloremque laudantium, totam rem aperiam, eaque ipsa
-              quae ab illo ed ut perspiciatis unde omnis iste natus error sit
-              voluptatem
-            </p>
+            <h5>Contact Information</h5>
+            <p>121 King Street, London, United Kingdom</p>
+            <p>1.800.458.556</p>
           </section>
         </div>
       </div>
